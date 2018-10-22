08a0383040e92c705020c6525125527792245c6a
diegosanchez <diego.sanchez@mercadolibre.com>
2018-10-21 23:33:06 -0300

    Adding list of Holidays

--- src/Holidays.hs
+++ src/Holidays.hs
@@ -3,10 +3,34 @@ module Holidays where
 import Data.Time.Calendar
 import Data.Time.Calendar.WeekDate
 
-data Holiday = Puntual Integer Int Int | Sunday | None 
+data Holiday = 
+    Puntual Integer Int Int | 
+    Sunday | 
+    RegardlessYear { month :: Int, day :: Int } |
+    Since { year :: Integer, month :: Int, day :: Int }
+    deriving (Show)
+                    
 
 isHoliday:: Holiday -> Day -> Bool
+
 isHoliday (Puntual year month day) date = date == fromGregorian year month day
+
 isHoliday Sunday date = weekDay == 7
     where (_, _, weekDay) = toWeekDate date 
+
+isHoliday RegardlessYear{month = m, day = d} date = m == monthOfDate && d == dayOfDate 
+    where
+        (_, monthOfDate, dayOfDate) = toGregorian date
+
+isHoliday Since{year = y, month = m, day = d} date = 
+    y >= yearOfDate && isHoliday RegardlessYear { month = monthOfDate, day = dayOfDate} date 
+    where
+        (yearOfDate, monthOfDate, dayOfDate) = toGregorian date
+
 isHoliday None _ = False
+
+isDateHoliday :: Day -> Holiday -> Bool
+isDateHoliday d h = isHoliday h d
+
+anyHoliday:: [Holiday] -> Day -> Bool
+anyHoliday l date = any (isDateHoliday date) l
--- test/HolidaySpec.hs
+++ test/HolidaySpec.hs
@@ -14,10 +14,30 @@ spec = do
     it "returns true whether puntual holiday is specified" $ do
         isHoliday (Puntual 2018 10 16) (fromGregorian 2018 10 16) `shouldBe` True
     it "returns false whether there not holidays at all" $ do
-        isHoliday (None) (fromGregorian 2018 10 16) `shouldBe` False
+        anyHoliday [] (fromGregorian 2018 10 16) `shouldBe` False
     it "returns false whether there not holidays at all #2" $ do
-        isHoliday (None) (fromGregorian 2018 10 16) `shouldBe` False
+        anyHoliday [] (fromGregorian 2018 10 16) `shouldBe` False
     it "returns true whether day of week is Sunday" $ do
         isHoliday (Sunday) (fromGregorian 2018 10 21) `shouldBe` True
     it "returns false whether day of week is not Sunday" $ do
         isHoliday (Sunday) (fromGregorian 2018 10 20) `shouldBe` False
+    it "returns true for Chrismas regardless year" $ do
+        isHoliday (RegardlessYear { month = 12, day = 25 }) (fromGregorian 2018 12 25) `shouldBe` True
+    it "returns true for Chrismas regardless year" $ do
+        isHoliday (RegardlessYear { month = 12, day = 25 }) (fromGregorian 2018 12 25) `shouldBe` True
+    it "returns false before '83" $ do
+        isHoliday (Since { year = 1983, month = 4, day = 2 }) (fromGregorian 1903 4 2) `shouldBe` True
+    it "returns true since '83" $ do
+        isHoliday (Since { year = 1983, month = 4, day = 2 }) (fromGregorian 1983 4 2) `shouldBe` True
+    it "returns false because there aren't holidays at all" $ do 
+        let calendar = []
+        anyHoliday calendar (fromGregorian 2018 10 22) `shouldBe` False
+    it "returns true because there is sunday on calendar" $ do 
+        let calendar = [Sunday]
+        anyHoliday calendar (fromGregorian 2018 10 21) `shouldBe` True
+    it "returns false because there are Since and RegardlessYear holidays but not Sunday" $ do 
+        let calendar = [ Since { year = 1983, month = 4, day = 2 } , RegardlessYear { month = 12, day = 25 } ]
+        anyHoliday calendar (fromGregorian 2018 10 21) `shouldBe` False
+    it "returns true since '83 even with list of holidays" $ do
+        anyHoliday calendar (fromGregorian 1983 4 2) `shouldBe` True
+        where calendar = [ Since { year = 1983, month = 4, day = 2 }, RegardlessYear { month = 12, day = 25 } ]
